fe73330a6e629dad0f652d768ab0e2f9559e5899
Ion Batîr <ionbatir@gmail.com>
2019-12-11 19:01:11 +0200

    Refactor App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import React from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { auth } from "firebase";
+import { Conversations, Messages } from "./components";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+const uiConfig = {
+  signInFlow: "popup",
+  signInSuccessUrl: false,
+  signInOptions: [auth.EmailAuthProvider.PROVIDER_ID],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false
+  }
+};
+
 function App() {
-  return <h1>Hello world!</h1>;
+  const [signedIn, setSignedIn] = React.useState(false);
+  const [conversation, setConversation] = React.useState(null);
+
+  React.useEffect(
+    () =>
+      auth().onAuthStateChanged(user =>
+        user ? setSignedIn(true) : setSignedIn(false)
+      ),
+    []
+  );
+
+  return signedIn ? (
+    <div class="container">
+      <div class="messaging">
+        <div class="inbox_msg">
+          <Conversations
+            selectConversation={conversation => setConversation(conversation)}
+            currentConversation={conversation}
+          />
+          {conversation && <Messages conversation={conversation} />}
+        </div>
+      </div>
+    </div>
+  ) : (
+    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth()} />
+  );
 }
 
 export default App;
