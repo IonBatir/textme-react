5b527ee157da3a260216a17f70f5a5b62223996c
Ion Batîr <ionbatir@gmail.com>
2019-12-11 18:57:19 +0200

    Add Conversations component

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -1,8 +1,12 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 import { fetchPersonalConversations } from "../api";
 import { getFormatedDate } from "../utils";
 
-export default function Conversations() {
+export default function Conversations({
+  selectConversation,
+  currentConversation
+}) {
   const [conversations, setConversations] = React.useState({
     data: [],
     loading: true
@@ -22,25 +26,37 @@ export default function Conversations() {
   );
 
   return (
-    <div class="inbox_chat">
-      {conversations.data.map(conversation => (
-        <div class="chat_list active_chat">
-          <div class="chat_people">
-            <div class="chat_img">
-              <img src={conversation.avatar} alt={conversation.name} />
-            </div>
-            <div class="chat_ib">
-              <h5>
-                {conversation.name}{" "}
-                <span class="chat_date">
-                  {getFormatedDate(conversation.lastTimestamp)}
-                </span>
-              </h5>
-              <p>{conversation.lastMessage}</p>
+    <div class="inbox_people">
+      <div class="headind_srch">
+        <div class="recent_heading">
+          <h4>Recent</h4>
+        </div>
+        <SearchBar />
+      </div>
+      <div class="inbox_chat">
+        {conversations.data.map(conversation => (
+          <div
+            onClick={() => selectConversation(conversation)}
+            class={`chat_list ${currentConversation === conversation &&
+              "active_chat"}`}
+          >
+            <div class="chat_people">
+              <div class="chat_img">
+                <img src={conversation.avatar} alt={conversation.name} />
+              </div>
+              <div class="chat_ib">
+                <h5>
+                  {conversation.name}
+                  <span class="chat_date">
+                    {getFormatedDate(conversation.lastTimestamp)}
+                  </span>
+                </h5>
+                <p>{conversation.lastMessage}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
